4494d85ee1d31524ba32dbe3e6726977e2f104b4
nkoldassov <koldassovnt@gmail.com>
2022-11-19 14:53:40 +0600

    Initial commit

---
+++ src/main/java/kz/nkoldassov/nerdapi/NerdApiApplication.java
@@ -0,0 +1,13 @@
+package kz.nkoldassov.nerdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdApiApplication.class, args);
+	}
+
+}
