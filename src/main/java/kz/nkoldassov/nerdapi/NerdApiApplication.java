07b56dcef377b6968060e00cdb6e567772ead319
nkoldassov <koldassovnt@gmail.com>
2022-11-26 20:21:28 +0600

    feat: logging set up

--- src/main/java/kz/nkoldassov/nerdapi/NerdApiApplication.java
+++ src/main/java/kz/nkoldassov/nerdapi/NerdApiApplication.java
@@ -1,10 +1,22 @@
 package kz.nkoldassov.nerdapi;
 
+import kz.nkoldassov.nerdapi.beans.all.ShowConfigValuesOnStartup;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class NerdApiApplication {
+
+	@Autowired
+	private ShowConfigValuesOnStartup showConfigValuesOnStartup;
+
+	@PostConstruct
+	public void init() {
+		showConfigValuesOnStartup.show();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NerdApiApplication.class, args);
